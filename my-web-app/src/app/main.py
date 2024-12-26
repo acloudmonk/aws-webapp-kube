5d1a9c3ccb7670707941b2b35eaa9d724ba91181
Sachin Singh <49566909+acloudmonk@users.noreply.github.com>
2024-12-26 15:22:21 +0530

    Update main.py

--- my-web-app/src/app/main.py
+++ my-web-app/src/app/main.py
@@ -1,1 +1,15 @@
+from flask import Flask
+import serverless_wsgi
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+def lambda_handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
